a81cb6f6004d2c62b29870dd82c987b1bf7d124b
Ryan Barry <ryanc.barry@yahoo.com>
2014-01-21 15:19:35 -0800

    Testing add method

--- test/index.js
+++ test/index.js
@@ -4,18 +4,23 @@ var Octopie = require('../index.js');
 
 
 describe('Octopie', function() {
-  var octopie;
   it('should require a name', function () {
     assert.throws(function () {
-      octopie = new Octopie();
+      var octopie = new Octopie();
     });
     assert.throws(function () {
-      octopie = new Octopie({ name:'foo' });
+      var octopie = new Octopie({ name:'foo' });
     });
   });
   it('should require an auth token', function () {
     assert.doesNotThrow(function () {
-      octopie = new Octopie({ name:'foo', authToken:'bar' });
+      var octopie = new Octopie({ name:'foo', authToken:'bar' });
     });
   });
+  it('should add repositories', function () {
+    var octopie = new Octopie({ name:'foo', authToken:'bar' });
+    octopie.add('fullscreeninc/bacon');
+    assert.deepEqual(octopie.repos, [{ "name":"fullscreeninc/bacon" }]);
+  });
+  // it('should add ')
 });
