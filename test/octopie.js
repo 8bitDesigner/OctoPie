4dfdc3c589ce2e5002871c91c1af67989d3e6e2c
Paul Sweeney <paul@8-bitdesign.com>
2014-01-23 10:54:44 -0800

    Removing event object

--- test/octopie.js
+++ test/octopie.js
@@ -1,32 +1,30 @@
 var assert = require("assert")
+  , sinon = require("sinon")
+
 var Octopie = require('../lib/octopie');
 
-function octoFactory(name, token, url) {
+function octoFactory(token, url) {
   // For easy testing - preconfigured objects
-  if (!name && !token && !url) {
-    name = 'foo'
+  if (!token && !url) {
     token = 'bar'
     url = 'http://google.com'
   }
 
-  return new Octopie({ name: name, authToken: token, url: url })
+  return new Octopie({ authToken: token, url: url })
 }
 
 describe('Octopie', function() {
-  it('should require a name, authToken, and URL', function () {
-    assert.throws(function () { new Octopie(); });
-    assert.throws(function () { new Octopie({ name:'foo' }); });
-    assert.throws(function () { new Octopie({ name:'foo', authToken: 'bar' }); });
-    assert.throws(function () { new Octopie({ authToken: 'bar', url: 'google.com' }); });
+  it('should require an authToken, and URL', function () {
+    assert.throws(function() { new Octopie(); });
+    assert.throws(function() { new Octopie({url: 'foo'}); });
+    assert.throws(function() { new Octopie({authToken: 'foo'}); });
 
-    assert.doesNotThrow(function () {
-      new Octopie({ name: 'foo', authToken: 'bar', url: 'google.com' });
-    });
+    assert.doesNotThrow(function() { new Octopie({url: 'foo', authToken: 'bar'}) })
   });
 
   it('should be callable without using the new operator', function() {
-    var withNew = new Octopie({ name: 'foo', authToken: 'bar', url: 'baz' })
-    var withoutNew = Octopie({ name: 'foo', authToken: 'bar', url: 'baz' })
+    var withNew = new Octopie({ authToken: 'bar', url: 'baz' })
+    var withoutNew = Octopie({ authToken: 'bar', url: 'baz' })
 
     assert(withNew instanceof Octopie)
     assert(withoutNew instanceof Octopie)
@@ -45,19 +43,51 @@ describe('Octopie', function() {
     assert.deepEqual(octopie.repos, ["fullscreeninc/bacon"]);
   })
 
-  it("should allow you to create event listeners", function() {
+  it("should keep track of the names of bound events", function() {
     var octopie = octoFactory()
-    fooCb = function fooCb() {}
-    barCb1 = function barCb1() {}
-    barCb2 = function barCb2() {}
 
-    octopie.on('foo', fooCb);
-    octopie.on('bar', barCb1);
-    octopie.on('bar', barCb2);
+    octopie.on('bar', function() {});
+    octopie.on('baz', function() {});
+    octopie.on('foo', function() {});
 
-    assert.equal(octopie.events.length, 2)
-    assert.deepEqual(octopie.events[0].callbacks, [fooCb])
-    assert.deepEqual(octopie.events[1].callbacks, [barCb1, barCb2])
+    assert.deepEqual(octopie.events, ['bar', 'baz', 'foo'])
+  })
+
+  it("shouldn't allow you to add the same event twice", function() {
+    var octopie = octoFactory()
+
+    octopie.on('bar', function() {});
+    octopie.on('bar', function() {});
+    octopie.on('foo', function() {});
+
+    assert.deepEqual(octopie.events, ['bar', 'foo'])
+  })
+
+  it("should keep the events array sorted alphabetically", function() {
+    var octopie = octoFactory()
+
+    octopie.on('bar', function() {});
+    octopie.on('foo', function() {});
+    octopie.on('baz', function() {});
+
+    assert.deepEqual(octopie.events, ['bar', 'baz', 'foo'])
+  })
+
+  it("should bind callbacks onto the underlying server instance", function() {
+    var octopie = octoFactory()
+      , fn1 = function() {}
+      , fn2 = function() {}
+      , fn3 = function() {}
+
+    sinon.spy(octopie.server, 'on')
+
+    octopie.on('bar', fn1);
+    octopie.on('foo', fn2);
+    octopie.on('baz', fn3);
+
+    assert(octopie.server.on.calledWith('bar', fn1))
+    assert(octopie.server.on.calledWith('foo', fn2))
+    assert(octopie.server.on.calledWith('baz', fn3))
   })
 
   it('should configure hooks', function () {
